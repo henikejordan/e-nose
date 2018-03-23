b11aaec69cfd027cd742b970af922d6e6728394b
henikejordan <henike_jordan@hotmail.com>
2018-03-22 21:08:22 -0300

    Add MQ-2

--- src/modelo/Gases.java
+++ src/modelo/Gases.java
@@ -13,13 +13,13 @@ public class Gases implements Sensor {
 
     @Override
     public int[] getIndices() {
-        int[] ret = {0, 1, 2, 3, 4, 5, 6, 7};
+        int[] ret = {13, 0, 1, 2, 3, 4, 5, 6, 7};
         return ret;
     }
 
     @Override
     public String[] getInfo() {
-        String[] ret = {"MQ-3", "MQ-4", "MQ-5", "MQ-6", "MQ-7", "MQ-8", "MQ-9", "MQ-135"};
+        String[] ret = {"MQ-2", "MQ-3", "MQ-4", "MQ-5", "MQ-6", "MQ-7", "MQ-8", "MQ-9", "MQ-135"};
         return ret;
     }
 
